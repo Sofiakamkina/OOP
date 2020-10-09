6169541daa51919d4d54c9fbadc2f2d5b9e926d5
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2020-10-09 22:37:53 +0700

    Update main.cpp

--- C++/lab0/main.cpp
+++ C++/lab0/main.cpp
@@ -1,4 +1,4 @@
-  #include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -19,7 +19,6 @@ public:
         std::string buffer, word;
         while(!input.eof()) {
             std::getline(input, buffer);
-
             for(size_t i = 0; i < buffer.size(); ++ i) {
                 if((buffer[i] > 47 && buffer[i] < 58) || (buffer[i] > 64  && buffer[i] < 91) ||
                         (buffer[i] > 96 && buffer[i] < 123)) {
@@ -39,20 +38,20 @@ public:
     }
 
     void ParsingWords() {
-        std::vector<std::pair<std::string, std::size_t> > sortFrequency;
+        std::vector<std::pair<std::string, size_t> > sortFrequency;
 
         for(const auto& elem : frequencyWords) {
             sortFrequency.emplace_back(make_pair(elem.first, elem.second));
         }
 
         sort(begin(sortFrequency), end(sortFrequency),
-             [](const std::pair<std::string, std::size_t>& lhs,
-                     const std::pair<std::string, std::size_t>& rhs) {
+             [](const std::pair<std::string, size_t>& lhs,
+                     const std::pair<std::string, size_t>& rhs) {
                  return lhs.second > rhs.second;
              });
 
         for(const auto& elem : sortFrequency) {
-            output << elem.first << "," <<	elem.second << ","
+            output << elem.first << "," << elem.second << ","
                    << 100 * elem.second / (float)words << "%" << std::endl;
         }
     }
@@ -65,7 +64,7 @@ private:
     std::ifstream input;
     std::ofstream output;
     size_t words;
-    std::map<std::string, std::size_t> frequencyWords;
+    std::map<std::string, size_t> frequencyWords;
 };
 
 int main(int argc, char* argv[]) {
