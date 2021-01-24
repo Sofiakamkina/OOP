eed80db1363aaccf511ef9a0ef37a87fd437c536
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-24 18:34:50 +0700

    Create consts.h

---
+++ lab2/consts.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <stack>
+#include <map>
+namespace  consts
+{
+
+    const int WithoutNameOfFile = 1;
+    const int IndexInputFile = 2;
+    const int BinaryOperation = 2;
+    const char COMMENTS = '#';
+    const char DOT = '.';
+    const char SPACE = ' ';
+    const int IndexComment = 0;
+    const int ONESPACE = 1;
+    const int TWOSPACES = 2, NULLSPACES = 0;
+}
