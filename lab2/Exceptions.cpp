2ee31c5c41b76de0c1e9cff51fec060fbaca98e9
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-24 19:11:20 +0700

    Create Exceptions.cpp

---
+++ lab2/Exceptions.cpp
@@ -0,0 +1,35 @@
+#include "Exceptions.h"
+
+const char * exceptions::WrongNumberOfParamter::what() const noexcept
+{
+    return "You must write 3 parameters. You didn't do this.";
+}
+
+const char * exceptions::OpenFile::what() const noexcept
+{
+    return "I can't open input or output file. Please, check names of file.\n";
+}
+
+const char* exceptions::WrongNameOfOperation::what() const noexcept
+{
+    return "I don't understand you\n";
+}
+const char* exceptions::DivizionByZero::what() const noexcept
+{
+    return "Dividion by zero\n";
+}
+
+const char* exceptions::StackIsEmpty::what() const noexcept
+{
+    return  "Stack is empty. Can't do \"pop\".";
+}
+
+const char* exceptions::notEnoughParameters::what() const noexcept
+{
+    return "Stack has't enough numbers. \n";
+}
+
+const char* exceptions::InvalidOperands::what() const noexcept
+{
+    return "invalid operands\n";
+}
