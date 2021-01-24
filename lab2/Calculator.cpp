04b4150f2749f9377884037812266d556bcb3b9f
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-24 19:12:51 +0700

    Create  Calculator.cpp

---
+++ lab2/Calculator.cpp
@@ -0,0 +1,54 @@
+#include "Calculator.h"
+#include "Exceptions.h"
+#include "consts.h"
+Calculator::Calculator(int argc, char* argv[])
+{
+    switch(argc)
+    {
+        case consts::WithoutNameOfFile:
+            input = new InputConsole;
+            break;
+        case consts::IndexInputFile:
+            input = new InputFile(argv[consts::IndexInputFile]);
+            break;
+        default:
+            throw exceptions::WrongNumberOfParamter();
+    }
+}
+
+void Calculator::run()
+{
+    Context a;
+    programOn = true;
+    while(programOn &&  input->hasMoreInput())
+    {
+        try
+        {
+            string inputLine = input->getCommandLine();
+            if(inputLine.empty())
+                closeProgram();
+            else
+                iprocessor.calculate(inputLine, &a);
+        }
+        catch(exceptions::WrongNameOfOperation &e)
+        {
+            std::cerr<<e.what();
+        }
+        catch(exceptions::StackIsEmpty &e)
+        {
+            std::cerr<<e.what();
+        }
+        catch(exceptions::notEnoughParameters &e)
+        {
+            std::cerr<<e.what();
+        }
+    }
+    closeProgram();
+
+}
+
+void Calculator::closeProgram()
+{
+    programOn = false;
+    input->closeInput();
+}
