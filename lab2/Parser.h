c5f750f66efcd0ff734cf848d2d2eed01fbdcbe2
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-24 18:35:22 +0700

    Create Parser.h

---
+++ lab2/Parser.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include "consts.h"
+#include "Operator.h"
+#include <vector>
+#include <string>
+using std::string;
+using std:: map;
+class Parser
+{
+private:
+public:
+    std::vector<string> parser(string str);
+};
