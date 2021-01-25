a5ea0a1096df4da1ca14339d766777f162a931da
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-25 22:03:43 +0700

    Update Iprocessor.cpp

--- lab2/Iprocessor.cpp
+++ lab2/Iprocessor.cpp
@@ -14,6 +14,17 @@ void Iprocessor::calculate(std::string &commandline, Context *context)
     if(tokens[consts::IndexComment].empty())
         return;
     OperatorFactory operatorFactory;
+
+    static Common::ShapeMaker<Pop> popMaker("pop");
+    static Common::ShapeMaker<Push> pushMaker("push");
+    static Common::ShapeMaker<Plus> plusMaker("+");
+    static Common::ShapeMaker<Minus> minusMaker("-");
+    static Common::ShapeMaker<Division> divisionMaker("/");
+    static Common::ShapeMaker<Multiplication> multiplicationMaker("*");
+    static Common::ShapeMaker<Sqrt> sqrtMaker("sqrt");
+    static Common::ShapeMaker<Print> printMaker("print");
+    static Common::ShapeMaker<Definition> defineMaker("define");
+
     std::shared_ptr<Operator> action = operatorFactory.getOperationType(tokens, context);
     action->calculate(context);
 }
