e8f0906210ad51dcace193dc037f7a14083512dc
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-25 21:59:46 +0700

    Update OperatorFactory.cpp

--- lab2/OperatorFactory.cpp
+++ lab2/OperatorFactory.cpp
@@ -2,25 +2,36 @@
 
 OperatorFactory::OperatorFactory()
 {
-    listOfOerators.emplace("pop", std::make_shared<Pop>());
-    listOfOerators.emplace("push", std::make_shared<Push>());
-    listOfOerators.emplace("+", std::make_shared<Plus>());
-    listOfOerators.emplace("-", std::make_shared<Minus>());
-    listOfOerators.emplace("/", std::make_shared<Division>());
-    listOfOerators.emplace("*", std::make_shared<Multiplication>());
-    listOfOerators.emplace("sqrt", std::make_shared<Sqrt>());
-    listOfOerators.emplace("print", std::make_shared<Print>());
-    listOfOerators.emplace("define", std::make_shared<Definition>());
 }
-std::shared_ptr<Operator> OperatorFactory::getOperationType(std::vector<string> tokens, Context *context)
+
+OperatorFactory& OperatorFactory::Instance()
 {
-    if(listOfOerators.find(tokens[0]) == listOfOerators.end())
+    static OperatorFactory factory;
+    return factory;
+}
+
+Operator* OperatorFactory::getOperationType(std::vector<string> tokens, Context *context)
+{
+    auto i = _makers.find(tokens[0]);
+    if (i == _makers.end())
+    {
         throw exceptions::WrongNameOfOperation();
+    }
+    IShapeMaker* maker = i->second;
 
-    p = listOfOerators.at(tokens[0]);
-    first = Operand(tokens[1],context->definitions);
-    second = Operand(tokens[2],context->definitions);
+    Operator* operator = maker->Create();
+    Operand first = Operand(tokens[1],context->definitions);
+    Operand second = Operand(tokens[2],context->definitions);
 
-    p->setOperands(first, second);
-    return p;
+    operator->setOperands(first, second);
+    return operator;
+}
+
+void registerOperationTypeMaker(const std::string& operatorName, IOperatorMaker* maker)
+{
+    if (_makers.find(key) != _makers.end())
+    {
+        throw new std::exception("Multiple makers for given key!");
+    }
+    _makers[key] = maker;
 }
