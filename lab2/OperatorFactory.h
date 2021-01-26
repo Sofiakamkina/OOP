0e639fdcb70f41ed015064e3392cd90726aaee32
s.kamkina <71336878+Sofiakamkina@users.noreply.github.com>
2021-01-26 11:41:30 +0700

    Update OperatorFactory.h

--- lab2/OperatorFactory.h
+++ lab2/OperatorFactory.h
@@ -1,17 +1,24 @@
 #pragma once
 
 #include <memory>
-#include"Operator.h"
-class OperatorFactory
+#include "Operator.h"
+
+class IOperatorTypeMaker
 {
+public:
+    virtual Operator * Create() const = 0;
+    virtual ~IOperatorTypeMaker() {}
+};
+
+class OperatorFactory {
 private:
-    std::map<std::string, Operator*> makers;
+    std::map<std::string, IOperatorTypeMaker *> makers;
 public:
     OperatorFactory();
 
-    static OperatorFactory& Instance();
+    static OperatorFactory &Instance();
 
-    Operator* getOperationType(std::vector<string> tokens, Context *context);
+    Operator *getOperationType(std::vector<string> tokens, Context *context);
 
-    void registerOperationTypeMaker(const std::string& operatorName, IOperatorMaker* maker);
+    void registerOperationTypeMaker(const std::string &operatorName, IOperatorTypeMaker *maker);
 };
